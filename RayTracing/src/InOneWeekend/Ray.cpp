030bf23235f38620eb92e65ba0882e1606d34970
PixlBrosG <pixelbrosg@gmail.com>
2022-01-18 00:49:53 +0100

    Surface normals + simplified Ray::HitSphere

--- RayTracing/src/InOneWeekend/Ray.cpp
+++ RayTracing/src/InOneWeekend/Ray.cpp
@@ -1,4 +1,5 @@
 #include <glm/geometric.hpp>
+#include <glm/gtx/norm.hpp>
 
 #include "InOneWeekend/Ray.h"
 
@@ -11,25 +12,34 @@ namespace RayTracing {
 
 	glm::vec3 Ray::GetColor() const
 	{
-		if (HitSphere(glm::vec3{ 0, 0, -1 }, 0.5f))
-			return glm::vec3{ 1, 0, 0 };
+		float t = HitSphere(glm::vec3{ 0, 0, -1 }, 0.5f);
+
+		if (t > 0.0f)
+		{
+			glm::vec3 N = glm::normalize(At(t) - glm::vec3{ 0, 0, -1 });
+			return 0.5f * glm::vec3{ N.r + 1, N.g + 1, N.b + 1 };
+		}
 
 		glm::vec3 unitDirection = glm::normalize(m_Direction);
-		float t = 0.5f * (unitDirection.y + 1.0f);
+		t = 0.5f * (unitDirection.y + 1.0f);
 		return (1.0f - t) * glm::vec3{ 1.0f, 1.0f, 1.0f } + t * glm::vec3{ 0.5f, 0.7f, 1.0f };
 	}
 
-	bool Ray::HitSphere(const glm::vec3& center, float radius) const
+	float Ray::HitSphere(const glm::vec3& center, float radius) const
 	{
 		glm::vec3 oc = m_Origin - center;
 
-		float a = glm::dot(m_Direction, m_Direction);
-		float b = 2.0f * glm::dot(oc, m_Direction);
-		float c = glm::dot(oc, oc) - radius * radius;
+		// dot(P,P) = squared length of P
+		float a = glm::length2(m_Direction);
+		float halfB = glm::dot(oc, m_Direction);
+		float c = glm::length2(oc) - radius * radius;
 
-		float discriminant = b * b - 4 * a * c;
+		float discriminant = halfB * halfB - a * c;
 
-		return discriminant > 0;
+		if (discriminant < 0)
+			return -1.0f;
+		else
+			return (-halfB - glm::sqrt(discriminant)) / a;
 	}
 
 }
--- RayTracing/src/InOneWeekend/Ray.h
+++ RayTracing/src/InOneWeekend/Ray.h
@@ -16,7 +16,7 @@ namespace RayTracing {
 		glm::vec3 At(float t) const { return m_Origin + t * m_Direction; }
 		glm::vec3 GetColor() const;
 
-		bool HitSphere(const glm::vec3& center, float radius) const;
+		float HitSphere(const glm::vec3& center, float radius) const;
 	public:
 		glm::vec3 m_Origin;
 		glm::vec3 m_Direction;
